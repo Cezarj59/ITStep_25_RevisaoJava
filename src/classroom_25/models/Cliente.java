05863f2a545eb9f48f11bf924baf666579c1b2f5
cezarj59 <cezarj59@yahoo.com>
2022-12-12 23:39:20 -0300

    add classe Cliente

---
+++ src/classroom_25/models/Cliente.java
@@ -0,0 +1,57 @@
+package classroom_25.models;
+
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nome, String cpf, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "\nID: " + id + "\nNome: " + nome + "\nCPF: " + cpf + "\nTelefone: " + telefone;
+    }
+
+}
